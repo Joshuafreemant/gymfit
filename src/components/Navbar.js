fe523677e2fc2cf5e28e8d4cc5e9807006ee68a2
Akinniyi Tolulope <47461817+Joshuafreemant@users.noreply.github.com>
2022-09-26 13:07:00 -0700

    Update Navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,7 +1,6 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 import { Stack } from '@mui/material'
-import { HashLink } from "react-router-hash-link";
 
 import Logo from '../assets/images/Logo.png';
 
@@ -24,7 +23,7 @@ const Navbar = () => {
         alignItems="flex-end"
       >
         <Link to="/" style={{ textDecoration: 'none', color: "#3A1212", borderBottom: '3px solid #FF2625' }} >Home</Link>
-        <HashLink href="/home#exercises" style={{ textDecoration: 'none', color: "#3A1212" }}>Exercises</HashLink>
+        <a href="/#exercises" style={{ textDecoration: 'none', color: "#3A1212" }}>Exercises</a>
       </Stack>
     </Stack>
   )
